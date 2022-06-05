469680982ca6daa0558906f98436393938fcc76b
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-05 11:37:49 +0700

    Create footer and add react-icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import {
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoMail,
+} from "react-icons/io5";
 
 import ProfilePicture from "../assets/zaidan.png";
 
@@ -54,6 +60,55 @@ export default function Home() {
           </div>
         </section>
       </main>
+
+      <footer className="bg-dark py-8 text-white">
+        <ul className="mx-auto flex justify-center gap-4">
+          <li>
+            <a
+              href="mailto:salehzaidan64@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-primary focus:text-primary"
+            >
+              <IoMail size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/saleh-zaidan"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-primary focus:text-primary"
+            >
+              <IoLogoLinkedin size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/salehzaidan"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-primary focus:text-primary"
+            >
+              <IoLogoGithub size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/salehzaidan_"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-primary focus:text-primary"
+            >
+              <IoLogoInstagram size={20} />
+            </a>
+          </li>
+        </ul>
+        <p className="mt-4 text-center text-sm">
+          &copy; 2022 Saleh Zaidan <br />
+          All rights reserved
+        </p>
+      </footer>
     </div>
   );
 }
